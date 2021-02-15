992ad2671ef1f8bfb0a2a653d684218a82bfaafa
vramirez-dacartec <vramirez@dacartec.com>
2021-02-15 11:56:12 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# onlystringandnumberspattern
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/onlystringandnumberspattern)
---
+++ index.js
@@ -0,0 +1,3 @@
+// Import stylesheets
+import "./style.css";
+document.getElementById("buscador").pattern = "^(?=.{3,})(?:[A-Za-z0-9 ]*)$";
